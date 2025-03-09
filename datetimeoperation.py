be832dd002226d60f23debaec58379c1cfc06b53
DaiyaG <daiyadharm@gmail.com>
2025-03-09 10:57:09 +0000

    datetime

---
+++ datetimeoperation.py
@@ -0,0 +1,12 @@
+import datetime
+
+#using now() to get current time
+current_time = datetime.datetime.now()
+
+#Printing value of now.
+print ("Time now at West Brom is :", end = "")
+print (current_time)
+
+#print calendar of 2025
+import calendar
+print("\n", calendar.calendar(2025))
